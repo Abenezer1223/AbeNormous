154415d0b8a45dbde8857c84bfc840b53f2b585a
Abenezer1223 <abenezer1223@gmail.com>
2024-10-12 02:06:10 -0500

    n/a
    The ultimate gym training app built with react & tailwindCSS

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ export default function Hero() {
                 <p>IT'S TIME TO GET</p>
                 <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>Abe<span className='text-blue-400'>normous</span></h1>
             </div>
-            <p className='text-sm md:text-base font-light'>I hereby acknowledgement that I may become <span className='text-blue-400 font-medium'>unbelievably abenormous</span> and accept all risks of becoming the local <span className='text-blue-400 font-medium'>mass montrosity</span>, afflicted with severe body dismorphia, unable to fit through doors.</p>
+            <p className='text-sm md:text-base font-light'>I acknowledge that I may transform into a <span className='text-blue-400 font-medium'>true Bat-normous,</span> embracing the shadows of the night with my newfound strength. I accept the risks of becoming the local <span className='text-blue-400 font-medium'>mass Gotham Gladiator</span>, grappling with epic gains and possibly needing to swoop through doors like the Dark Knight himself!</p>
             <Button func={() => {
                 window.location.href = '#generate'
             }} text={"Accept & Begin"}></Button>
